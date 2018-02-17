cc9f45ad6e321afad188cc9885c1c1e6009e773b
Florencia De Vuono <flopy_dv@icloud.com>
2018-02-17 13:27:10 -0800

    new script file and css changes

---
+++ script.js
@@ -0,0 +1,7 @@
+[].forEach.call(document.querySelectorAll('img[data-src]'),
+function(img){
+  img.setAttribute('src', img.getAttribute('data-src'));
+  img.onload = function(){
+    img.removeAttribute('data-src');
+  };
+});
